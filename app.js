40bba69cfbc7190ae3e6293acd108623cef72501
ghulambaqir <gbaqir347@gmail.com>
2022-09-03 10:08:31 +0500

    add initial files

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+var items = ["WAKE UP","NETFLIX"]
+
+
+app.set('view engine', 'ejs')
+
+app.get("/", function(req,res){
+
+    var currentDay = new Date();
+
+    var options ={
+        weekday: "long",
+        day : "numeric",
+        month: "long"
+    }
+
+    var today = currentDay.toLocaleDateString("en-IN", options)
+
+    res.render("list",{
+        day:today,
+        ListItem: items
+    })
+
+    
+})
+
+app.post("/", function(req,res){
+  var item = req.body.newItem; 
+   items.push(item)
+   res.redirect("/")
+})
+
+app.listen(3000,function(){
+    console.log("SERVER STARTED ON PORT 3000");
+})
